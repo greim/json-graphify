b9606eefe43e68766888a9684cda21d8eb4362b5
Greg Reimer <greimer@tangogroup.com>
2015-12-01 23:06:02 -0700

    simplify index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,5 @@
 import Converter from './converter';
 
 export default function graphify(opts) {
-  const converter = new Converter(opts);
-  function toPathValues(json) {
-    return converter.toPathValues(json);
-  }
-  function toGraph(json) {
-    return converter.toGraph(json);
-  }
-  function toPathMap(json) {
-    return converter.toPathMap(json);
-  }
-  return Object.freeze({ toPathValues, toGraph, toPathMap });
+  return new Converter(opts);
 }
